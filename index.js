c447ef991518be6a0e5654378a128192736bcc3a
LanAleCoder <peter24alexander@gmail.com>
2022-08-23 19:04:02 -0600

    Conexion a heroku y mongodb atlas

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
 const app = require("./app");
-
+require('dotenv').config();
 //Conexion a la base de datos
 mongoose.Promise = global.Promise;
 mongoose
-  .connect("mongodb://localhost:27017/Interview", {
+  .connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.j0jig.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
     console.log("Conexion con la base de datos correcta");
 
-    app.listen(3000, () => {});
+    app.listen(process.env.PORT || 3000, () => {});
   })
   .catch((error) => console.log(error));
